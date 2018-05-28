0bb92ff56eeca436db3e2203cf9952cfebae82d6
Waqas <waqas@leno.ha>
2018-05-28 18:11:38 +0200

    commit

--- index.js
+++ index.js
@@ -8,16 +8,34 @@ app.get('/', (req, res) => {
 	res.send('GraphQL is amazing');
 });
 
-const root = { friend: () => {
-    return {
-        "id": 25874231,
-        "fname": "Larry",
-        "lname":"Jensen",
-        "lang":"en",
-        "email":"hello@juka.net",      
-    
+class Friend {
+	constructor(id, { firstName, lastName, gender, language, email }) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gender = gender;
+		this.language = language;
+		this.email = email;
+	}
+}
+
+const friendDB = {};
+const root = {
+	friend: () => {
+		return {
+			id: 25874231,
+			fname: 'Larry',
+			lname: 'Jensen',
+			lang: 'en',
+			email: [ { email: 'lala@lal.dk' }, { email: 'lahehe@lal.dk' } ],
+		}
+    },
+    createFriend: ({input}) => {
+        let id = require('crypto').randomBytes(10).toString('hex');
+        friendDB[id] = input;
+        return new Friend(id, input);
     }
-} }; //resolver must match model
+}; //resolver must match model
 
 app.use(
 	'/graphql',
--- schema.js
+++ schema.js
@@ -3,15 +3,32 @@ import { buildSchema } from 'graphql';
 const schema = buildSchema(`
 type Friend {
     id:ID
-    fname: String
-    lname: String
+    firstName: String
+    lastName: String
     gender: String
-    lang: String
+    language: String
     email: String
 }
 type Query {
     friend: Friend
 }
-`)
+
+
+
+input FriendInput {
+
+    id:ID
+    firstName: String!
+    lastName: String
+    gender: String
+    language: String
+    email: String
+
+}
+
+type Mutation{
+    createFriend(input: FriendInput): Friend
+}
+`);
 
 export default schema;
